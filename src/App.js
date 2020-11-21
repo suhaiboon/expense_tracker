3a5ca03f1c36ad626ed92f8aa84dec22fd5ceb32
Suhaiboon <suhaibtareq@gmail.com>
2020-11-21 08:44:54 -0500

    "Finished the Expense Tracker Tutorial"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
+import Header from "./components/Header";
+import { Balance } from "./components/Balance";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
+import { AddTransaction } from "./components/AddTransaction";
+import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <GlobalProvider className="App">
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
 }
 // testing pushing to github
 export default App;
-
-
-cleanirgkl miss agian   
